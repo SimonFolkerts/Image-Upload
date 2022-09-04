02dd1507d0ff1391639e0d1626749cdc3b7c2197
Simon Folkerts <simfolkerts@gmail.com>
2022-09-05 08:40:15 +1200

    add basic endpoints to server.js

--- back/server.js
+++ back/server.js
@@ -9,6 +9,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(req.method + " to " + req.url);
+});
+
+app.get("/data", function (req, res) {
+  res.send({ data: "get" });
+});
+
+app.post("/data", (req, res) => {
+  res.send({ data: "post" });
+});
+
+app.put("/data", (req, res) => {
+  res.send({ data: "put" });
+});
+
+app.delete("/data", (req, res) => {
+  res.send({ data: "delete" });
+});
+
 app.listen(port, () => {
   console.log("listening on port " + port + "...");
 });
